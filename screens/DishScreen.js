0adb35f0f58b3f737ba4154623f8f8f5c5d44d79
MokoneSA <simonlephotojr@gmail.com>
2023-10-27 20:42:59 +0200

    updated

--- screens/DishScreen.js
+++ screens/DishScreen.js
@@ -7,45 +7,19 @@ import { TouchableOpacity } from 'react-native';
 import { useDispatch } from 'react-redux';
 import { addToCart } from '../redux/slices/cartSlice';
 import { Checkbox } from 'react-native-paper';
+import { FlatList } from 'react-native-web';
 
 export default function DishScreen() {
 
     const [selectedExtras, setSelectedExtras] = useState([]);
-    const [checked, setChecked] = useState(false);
+    const [isChecked, setChecked] = useState(false);
+
 
     const { params } = useRoute();
     const dispatch = useDispatch()
     const navigation = useNavigation()
 
     let { item, extras } = params;
-
-
-
-    const extrasArray = Object.keys(extras).map((key) => ({
-        name: extras[key].name,
-        price: extras[key].price,
-    }));
-
-
-    const radio1 = {
-        name: extrasArray[1].name,
-        price: extrasArray[1].price,
-    }
-
-    const radio2 = {
-        name: extrasArray[0].name,
-        price: extrasArray[0].price,
-    }
-
-    console.log("Dish View: ", radio2.price)
-
-
-    // const extrasArray = Array.isArray(extras) ? extras.map(extra => ({
-    //     name: extra.name,
-    //     price: extra.price,
-    // })) : [];
-
-    // console.log("Dish View: ", extras)
 
     // handles increasing the number of items
     const handleAddToCart = () => {
@@ -57,10 +31,8 @@ export default function DishScreen() {
         const isSelected = selectedExtras.includes(extra);
 
         if (isSelected) {
-            // If the extra is already selected, remove it from the list
             setSelectedExtras(selectedExtras.filter((item) => item !== extra));
         } else {
-            // If it's not selected, add it to the list
             setSelectedExtras([...selectedExtras, extra]);
         }
     };
@@ -76,19 +48,28 @@ export default function DishScreen() {
                     <Text style={{ fontSize: 24, lineHeight: 36, marginTop: 50 }}>{item.name}</Text>
                 </View>
                 <View>
-                    <Text style={{ fontSize: 18, color: 'gray', marginHorizontal: 10, marginTop: 5 }}>{item.descr}</Text>
+                    <Text style={{ fontSize: 18, color: 'gray', marginHorizontal: 10, marginTop: 5, textAlign: 'left' }}>{item.descr}</Text>
                 </View>
-                <View style={styles.extraItem}>
-                    <Text>What would you like to add?</Text>
-                    <View>
-                        <Checkbox
-                            status={checked ? 'checked' : 'unchecked'}
-                            onPress={() => {
-                                setChecked(!checked);
-                            }}
-                            style={{ borderRadius: 10 }}
-                        />
-                    </View>
+                <View style={{ marginVertical: 10, width: '100%', marginHorizontal: 10 }}>
+                    <Text style={{ textAlign: 'left', marginHorizontal: 10, fontSize: 18 }}>What would you like to add?</Text>
+                </View>
+                <View style={{ width: '100%', marginHorizontal: 10 }}>
+                    {
+                        extras.map((extra, index) => (
+                            <View key={index} style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginHorizontal: 10, marginVertical: 10 }}>
+                                <TouchableOpacity
+                                    onPress={() => handleExtraSelection(extra)}
+                                    style={styles.checkbox}
+                                >
+                                    <Text>{selectedExtras.includes(extra) ? 'X' : ''}</Text>
+                                </TouchableOpacity>
+                                <View style={styles.extraInfo}>
+                                    <Text style={styles.extraName}>{extra.name}</Text>
+                                    <Text style={styles.extraPrice}>{extra.price}</Text>
+                                </View>
+                            </View>
+                        ))
+                    }
                 </View>
                 <View style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-end', marginBottom: 36 }}>
                     <TouchableOpacity
@@ -132,5 +113,27 @@ const styles = StyleSheet.create({
         borderTopLeftRadius: 30,
         borderTopRightRadius: 30,
         backgroundColor: 'white',
-    }
+    },
+    extraInfo: {
+        width: '80%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginHorizontal: 10,
+        marginVertical: 10,
+    },
+    checkbox: {
+        width: '7%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderWidth: 1,
+        marginHorizontal: 10,
+        marginVertical: 5,
+    },
+    extraName: {
+        fontSize: 20,
+    },
+    extraPrice: {
+        fontSize: 20,
+    },
 });
